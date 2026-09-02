bc2be3e19147fcfc14909e625b3f04361e0022f2
danghung04 <22050015@student.bdu.edu.vn>
2026-09-02 12:12:57 +0700

    Remove read_pdf.py script

--- read_pdf.py
+++
@@ -1,8 +0,0 @@
-import pdfplumber, sys
-sys.stdout.reconfigure(encoding='utf-8')
-with pdfplumber.open("ThieuDangHung_22050015 - DeCuongDATN.pdf") as pdf:
-    for i, page in enumerate(pdf.pages):
-        print(f"--- Page {i+1} ---")
-        t = page.extract_text()
-        if t:
-            print(t)
